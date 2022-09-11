157afd55c24a4ed487d7dc433702b4833ca281e3
Karlex1 <104476272+Karlex1@users.noreply.github.com>
2022-09-11 15:32:05 +0530

    j

--- src/component/Image.js
+++ src/component/Image.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 export const Image = () => {
   return (
-<<<<<<< HEAD
     <img src="https://t3.ftcdn.net/jpg/03/08/77/90/360_F_308779037_iftiqKoqVTDzTnG4t8SSnwnb4s6qRG20.jpg" alt="Profile_img" className='img img-thumbnail rounded float-start' />
-=======
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl1Ik3ygpdn8FncAwa98V63qE8SouDjhjRPg&usqp=CAU" alt="Profile_img" className='img img-thumbnail rounded float-start' />
->>>>>>> a9a31a0aa5fbac56264787637838c0c7198f5e1f
   )
 }
